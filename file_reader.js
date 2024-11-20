8aaa1c8a790632b9a5cebe7531bd0edce6cd18a6
GustavoFantin <gustavofantinbarros@gmail.com>
2024-11-20 11:59:46 -0800

    async/await and then/catch exercises

--- file_reader.js
+++ file_reader.js
@@ -1,10 +1,40 @@
 const fs = require("fs").promises;
 
-// THEN-CATCH SOLUTION BELOW THIS LINE
+// THEN-CATCH SOLUTION BELOW THIS LINE ///John Smith is 50 years old and his hobbies are swimming and dancing
+
+let firstname, lastname, hobbies, age
+fs.readFile('firstname.txt', 'utf-8' )
+    .then(data => {
+        firstname = data
+        return fs.readFile('lastname.txt', 'utf-8')
+    })
+    .then(data => {
+        lastname = data
+        return fs.readFile('age.txt', 'utf-8')
+    })
+    .then(data => {
+        age = data
+        return fs.readFile('hobbies.txt', 'utf-8')
+    })
+    .then(data => {
+        hobbies = JSON.parse(data)
+        console.log(`THEN-CATCH:    ${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+    
 
 
+// ASYNC/AWAIT SOLUTION BELOW THIS LINE 
+
+const waiting = async () => {
+    const firstname = await fs.readFile('firstname.txt', 'utf-8')
+    const lastname = await fs.readFile('lastname.txt', 'utf-8')
+    const age = await fs.readFile('age.txt', 'utf-8')
+    const hobbies = JSON.parse(await fs.readFile('hobbies.txt', 'utf-8'))
+    console.log(`ASYNC/AWAIT:    ${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`)
+}
 
 
-
-
-// ASYNC/AWAIT SOLUTION BELOW THIS LINE
+waiting()
